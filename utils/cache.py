e8ceff96b3c99be218da5f987c5c6f7a9c515b7c
sherwin-77 <sherwinak1@gmail.com>
2025-06-02 21:28:00 +0700

    Update cache util

--- utils/cache.py
+++ utils/cache.py
@@ -1,10 +1,11 @@
+from collections import OrderedDict
 import logging
 import discord
 from dataclasses import dataclass
 
-from typing import Dict, Optional, Union
+from typing import Any, Optional, Union
 
-from .structure import AsyncLinkedList, Node
+from .structure import Node
 
 logger = logging.getLogger(__name__)
 
@@ -16,22 +17,25 @@ handler.setLevel(logging.INFO)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+
 @dataclass
 class CacheData(Node):
-    def __init__(self, data: discord.Message, keyid: str, *, next_: Optional[Node] = None, prev: Optional[Node] = None) -> None:
+    def __init__(
+        self, data: discord.Message, keyid: str, *, next_: Optional[Node] = None, prev: Optional[Node] = None
+    ) -> None:
         super().__init__(data, next_=next_, prev=prev)
         self.keyid = keyid
+
 
 class MessageCache:
     """
     Represent Cache of :class:`discord.Message` using LRU strategy
     """
+
     def __init__(self, maxlen=500) -> None:
         if maxlen < 1:
             raise ValueError("Max length must be 1 or greater")
-        self.__cache: Dict[str, discord.Message] = {}
-        self.__record: Dict[str, CacheData] = {}
-        self.__q = AsyncLinkedList()
+        self.__cache = OrderedDict()
         self._maxlen = maxlen
         logger.debug("Created instance of MessageCache")
 
@@ -53,31 +57,20 @@ class MessageCache:
                 return message
         return None
 
-    async def add_message(self, message: discord.Message, custom_key: Optional[str] = None) -> None:
+    def add_message(self, message: discord.Message, custom_key: Optional[str] = None) -> None:
         if custom_key is not None and custom_key.startswith("message-"):
             raise KeyError("'message-' prefix is not allowed as custom key")
         logger.debug("Add message key %s", custom_key or message.id)
         # Add to hashmap
         self.__cache[custom_key or f"message-{message.id}"] = message
-        # Add to list (For LRU Cache deleting)
-        n = CacheData(message, custom_key or f"message-{message.id}")
-        self.__record[custom_key or f"message-{message.id}"] = n
-        await self.__q.add_node(n)
-        while len(self.__cache) > self.maxlen and self.__q.head is not None:
-            n: CacheData
-            n = self.__q.head  # type: ignore
-            self.__record.pop(n.keyid)
-            self.__cache.pop(n.keyid)
-            await self.__q.remove_node(n)
-            logger.debug("Performing auto delete message key %s", n.keyid)
+        self.__cache.move_to_end(custom_key or f"message-{message.id}")
 
+        while len(self.__cache) > self._maxlen:
+            self.__cache.popitem(last=False)
 
-    async def remove_message(self, key: Union[str, int]) -> Optional[discord.Message]:
+    def remove_message(self, key: Union[str, int]) -> Optional[discord.Message]:
         if isinstance(key, int):
             if f"message-{key}" in self.__cache:
-                if f"message-{key}" in self.__record:
-                    n = self.__record.pop(f"message-{key}")
-                    await self.__q.remove_node(n)
                 logger.debug("Removed message key %s", key)
                 return self.__cache.pop(f"message-{key}")
 
@@ -87,23 +80,67 @@ class MessageCache:
                     key_del = name
                     break
             if key_del is not None:
-                if f"message-{key_del}" in self.__record:
-                    n = self.__record.pop(f"message-{key_del}")
-                    await self.__q.remove_node(n)
-                logger.debug("Removed message custom key %s", key_del)
                 return self.__cache.pop(key_del)
         else:
             if key in self.__cache:
-                if key in self.__record:
-                    n = self.__record.pop(key)
-                    await self.__q.remove_node(n)
                 logger.debug("Removed message key %s", key)
                 return self.__cache.pop(key)
-        
+
         return None
-    
 
     def clear(self) -> None:
         self.__cache.clear()
-        self.__record.clear()
-        self.__q.clear()
+        logger.debug("Cleared cache")
+
+
+class LRUCache:
+    """
+    A generic Least Recently Used (LRU) cache implementation.
+    When the cache reaches its capacity, the least recently used item is removed.
+    """
+
+    def __init__(self, maxlen=100) -> None:
+        if maxlen < 1:
+            raise ValueError("Max length must be 1 or greater")
+        self.__cache = OrderedDict()
+        self._maxlen = maxlen
+        logger.debug("Created instance of LRUCache")
+
+    @property
+    def maxlen(self):
+        return self._maxlen
+
+    def get(self, key: str) -> Optional[Any]:
+        """Get an item from the cache. Returns None if not found."""
+        if key in self.__cache:
+            # Move to end to mark as recently used
+            self.__cache.move_to_end(key)
+            return self.__cache[key]
+        return None
+
+    def put(self, key: str, value: Any) -> None:
+        """Add an item to the cache or update if key exists."""
+        if key in self.__cache:
+            self.__cache.pop(key)
+        self.__cache[key] = value
+        self.__cache.move_to_end(key)
+
+        # Remove oldest item if we're over capacity
+        if len(self.__cache) > self._maxlen:
+            self.__cache.popitem(last=False)
+
+    def remove(self, key: str) -> Optional[Any]:
+        """Remove an item from the cache and return it."""
+        if key in self.__cache:
+            logger.debug("Removed item with key %s", key)
+            return self.__cache.pop(key)
+        return None
+
+    def clear(self) -> None:
+        """Clear all items from the cache."""
+        self.__cache.clear()
+        logger.debug("Cleared cache")
+
+    def __len__(self) -> int:
+        """Return the current size of the cache."""
+        return len(self.__cache)
