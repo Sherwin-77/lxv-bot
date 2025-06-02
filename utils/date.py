cfacc9613a70e78881353a01147b90f9564645c3
sherwin-77 <sherwinak1@gmail.com>
2025-06-02 21:35:41 +0700

    Update date util

--- utils/date.py
+++ utils/date.py
@@ -7,8 +7,8 @@ I hate timezones
 import datetime
 import calendar
 from typing import Optional
+import zoneinfo
 
-import pytz
 
 def add_months(date: datetime.datetime, months: int, reset_day: Optional[int] = None) -> datetime.datetime:
     month = date.month - 1 + months
@@ -18,14 +18,14 @@ def add_months(date: datetime.datetime, months: int, reset_day: Optional[int] = 
 
     return date.replace(year=year, month=month, day=day)
 
-def reset_tz(date: datetime.datetime, new_timezone: pytz.tzinfo.BaseTzInfo) -> datetime.datetime:
-    return new_timezone.localize(datetime.datetime(date.year, date.month, date.day, 0, 0, 0))
 
-def absolute_day_diff(date1: datetime.datetime, date2: datetime.datetime) -> int:
-    return abs(date1.astimezone(pytz.utc).replace(hour=0, minute=0, second=0, microsecond=0) - date2.astimezone(pytz.utc).replace(hour=0, minute=0, second=0, microsecond=0)).days
+def absolute_day_diff(date1: datetime.datetime, date2: datetime.datetime, tz: zoneinfo.ZoneInfo) -> int:
+    return abs(date1.astimezone(tz) - date2.astimezone(tz)).days
+
 
 def start_of_day(date: datetime.datetime) -> datetime.datetime:
     return date.replace(hour=0, minute=0, second=0, microsecond=0)
+
 
 def end_of_day(date: datetime.datetime) -> datetime.datetime:
     return date.replace(hour=23, minute=59, second=59, microsecond=999999)
